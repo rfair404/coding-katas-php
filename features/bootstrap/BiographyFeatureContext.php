977f051b04999d89a9cbada2b77003d66b5e8ebf
Russell Fair <rfair404@gmail.com>
2017-06-06 18:13:31 -0700

    ignore vendor

---
+++ features/bootstrap/BiographyFeatureContext.php
@@ -0,0 +1,13 @@
+<?php
+use Behat\Behat\Context\Context, Behat\Behat\Tester\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode, Behat\Gherkin\Node\TableNode;
+use CodingKatasPHP;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext implements Context
+{
+
+
+}

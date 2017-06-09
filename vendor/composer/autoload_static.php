5c983f29391ec997a2126bf22a02fcacec79ebf3
Russell Fair <rfair404@gmail.com>
2017-06-09 17:05:51 +0000

    update build on c9

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -8,8 +8,8 @@ class ComposerStaticInitdac0f086d44a4ac59af012e397f436a2
 {
     public static $files = array (
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
-        'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
         'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
+        'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
         '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
     );
 
